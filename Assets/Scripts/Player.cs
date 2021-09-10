4e4b7e79aad1e4d9041671c6150688e97ea60d5f
kit <kitbdev@gmail.com>
2021-09-10 18:01:48 -0400

    tmp and input system

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField] float moveSpeed = 2;
+
+    Rigidbody2D rb;
+
+    private void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+}
