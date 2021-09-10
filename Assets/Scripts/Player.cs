a5cca7331a308c3cb8095f1df69bca80f4fdd4a8
kit <kitbdev@gmail.com>
2021-09-10 18:08:06 -0400

    basic movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,7 +8,23 @@ public class Player : MonoBehaviour {
 
     Rigidbody2D rb;
 
+    Controls controls;
+    Vector2 inputMove;
+    bool inputShoot;
+
     private void Awake() {
         rb = GetComponent<Rigidbody2D>();
+        controls = new Controls();
+        controls.Enable();
+        controls.Player.Move.performed += c => inputMove = c.ReadValue<Vector2>();
+        controls.Player.Move.canceled += c => inputMove = Vector2.zero;
     }
+    private void FixedUpdate() {
+        // move
+        // todo acceleration
+        // todo bounds
+        Vector2 vel = inputMove * moveSpeed;
+        rb.velocity = vel;
+    }
+
 }
