1ed1907636fe42e69b9ff18b771ac0b48922714a
kit <kitbdev@gmail.com>
2021-09-10 20:37:06 -0400

    player acceleration and bounds

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,15 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-    [SerializeField] float moveSpeed = 2;
+    [SerializeField] float boundMaxX = 10;
+    [SerializeField] float boundMaxY = 0;
+    [SerializeField] float boundMinY = -5;
+    [SerializeField] float moveSpeed = 10;
+    [SerializeField] float accelerationRate = 2;
+    [SerializeField] Transform shootPoint;
+    [SerializeField] Transform shootPrefab;// todo
+
+    Vector2 velocity = Vector2.zero;
 
     Rigidbody2D rb;
 
@@ -20,11 +28,43 @@ public class Player : MonoBehaviour {
         controls.Player.Move.canceled += c => inputMove = Vector2.zero;
     }
     private void FixedUpdate() {
+
         // move
-        // todo acceleration
-        // todo bounds
-        Vector2 vel = inputMove * moveSpeed;
-        rb.velocity = vel;
+        Vector2 nvel = Vector2.ClampMagnitude(inputMove, 1f) * moveSpeed;
+        velocity = Vector2.Lerp(velocity, nvel, accelerationRate * Time.deltaTime);
+        // if (velocity.sqrMagnitude > 0.001f) {
+        // }
+        rb.velocity = velocity;
+
+        // keep player in bounds
+        Vector3 pos = transform.position;
+        if (pos.x > boundMaxX) {
+            pos.x = boundMaxX;
+            transform.position = pos;
+        } else if (pos.x < -boundMaxX) {
+            pos.x = -boundMaxX;
+            transform.position = pos;
+        }
+        pos = transform.position;
+        if (pos.y > boundMaxY) {
+            pos.y = boundMaxY;
+            transform.position = pos;
+        } else if (pos.y < boundMinY) {
+            pos.y = boundMinY;
+            transform.position = pos;
+        }
+
+    }
+    private void OnDrawGizmosSelected() {
+        Vector3 topleft = new Vector3(-boundMaxX, boundMaxY);
+        Vector3 topright = new Vector3(boundMaxX, boundMaxY);
+        Vector3 bottomleft = new Vector3(-boundMaxX, boundMinY);
+        Vector3 bottomright = new Vector3(boundMaxX, boundMinY);
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(topleft, topright);
+        Gizmos.DrawLine(topleft, bottomleft);
+        Gizmos.DrawLine(bottomright, bottomleft);
+        Gizmos.DrawLine(bottomright, topright);
     }
 
 }
---
+++ Assets/Scripts/ScrollingBackground.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour {
+
+    [SerializeField] float speed = 1;
+    [SerializeField] float height = 10;
+
+    private void Update() {
+        transform.position += Vector3.down * speed * Time.deltaTime;
+        // todo loop
+        if (transform.position.y < -height) {
+            transform.position = Vector3.zero;
+        }
+        // todo different screen sizes?
+    }
+}
